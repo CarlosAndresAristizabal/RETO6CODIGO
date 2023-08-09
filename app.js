2a807ab3acbfefb788c6716be74f644c3f37079c
Carlos Andres <carlosaristi22@gmail.com>
2023-08-09 08:03:28 -0500

    Iniciando proyecto Reto 6 Menú a la carta conexcion´a MongoDB

---
+++ app.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const url = 'mongodb://localhost/restaurante'
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('Conectado a MongoDB'))
+    .catch((e) => console.log('El error de conexión es: ' + e))
+
+const datosSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    address: {
+        building: { type: String },
+        coord: { type: [ Number ] },
+        street: { type: String },
+        zipcode: { type: String }
+    },
+    borough: { type: String },
+    cuisine: { type: String },
+    image: { type: String },
+    schedule: { type: String },
+    grades: [ {
+        date: { type: Date },
+        score: { type: Number },
+    } ],
+    comments: [ {
+        date: { type: Date },
+        comment: { type: String },
+    } ]
+});
+const datosModel = mongoose.model('comentarios', datosSchema)
+
+// Mostrar
+const mostrar = async () => {
+    const restaurante = await datosModel.find()
+    console.log(restaurante);
+}
+
+mostrar()
